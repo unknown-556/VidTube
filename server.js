809dc8c5941bf98bd026074d69b4bdcfaac25174
unknown-556 <167101661+unknown-556@users.noreply.github.com>
2024-06-12 04:03:37 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,27 +1,40 @@
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
-import router from './index.js'
+import dotenv from 'dotenv'
+import http from 'http'
+import router from './src/route/index.js'
+import connectDB from './database.js'
+
 
 const app = express()
+dotenv.config();
+
+const server = http.createServer(app)
+
 
 app.use(cors({origin:"*"}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
-app.use('/api/v3', router)
+app.use('/api/vidtube', router)
 
 
-const startServer  = async () => {
-    const PORT  = process.env.PORT || 2345
-    
-    try {
-       app.listen(PORT,() => {console.log(`APP IS RUNNING ON PORT: ${PORT}`);})
-    } catch (error) {
+
+
+
+const startServer = async () => {
+   const PORT = process.env.PORT || 5505;
+   connectDB();
+   try {
+       server.listen(PORT, () => {
+           console.log(`APP IS RUNNING ON PORT: ${PORT}`);
+       });
+   } catch (error) {
        console.log(error);
-    }
- };
+   }
+};
 
- startServer();
+startServer();
 
 app.get("/", (req,res) => {
    res.send('API IS RUNNING')
