686fa5d6088c14fbf159799e9d7000f24c8edae4
unknown-556 <167101661+unknown-556@users.noreply.github.com>
2024-06-19 03:15:27 -0700

    Update authRoute.js

--- authRoute.js
+++ authRoute.js
@@ -1,6 +1,1 @@
-import express from "express";
-const router = express.Router()
-import { signUp } from "./controllers/userControllers.js";
 
-router.post("/register",signUp)
-export default router;
